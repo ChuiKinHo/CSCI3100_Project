4771286395816aed2da3a8b12aacffda7e36b9e0
Nichole Chow <nicolechow08@gmail.com>
2023-03-24 12:38:37 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['help.twitter.com'],
+    domains: ["help.twitter.com", "https://pbs.twimg.com"],
   },
-}
+};
 
-                   module.exports = nextConfig
+module.exports = nextConfig;
