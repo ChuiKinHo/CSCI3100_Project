c4199ffe830a70a70d0934319616690f498bb6c3
Nichole Chow <nicolechow08@gmail.com>
2023-03-24 12:55:18 +0800

    Minor UI fix for Widget

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -35,7 +35,7 @@ export default function Widget() {
     },
   ];
   return (
-    <div div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
+    <div className="xl:w-[600px] hidden lg:inline ml-4 space-y-4">
       {/* <Search /> */}
       <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
         <div className="flex items-center p-3 rounded-full  relative">
