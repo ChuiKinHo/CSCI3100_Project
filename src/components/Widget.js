2ef402a87bb2c95a435747a34a65c701b9f1e32e
Nichole Chow <nicolechow08@gmail.com>
2023-03-24 12:50:31 +0800

    Update Widget.js for style fix

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,4 +1,5 @@
 import Search from "./Search.js";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 
 export default function Widget() {
   const randomUser = [
@@ -34,8 +35,18 @@ export default function Widget() {
     },
   ];
   return (
-    <div className="sticky top-16 text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[75%]">
-      <Search />
+    <div div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
+      {/* <Search /> */}
+      <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
+        <div className="flex items-center p-3 rounded-full  relative">
+          <MagnifyingGlassIcon className="h-5 z-50 text-gray-500" />
+          <input
+            className="absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100 "
+            type="text"
+            placeholder="Search Twitter"
+          />
+        </div>
+      </div>
       <h4 className="font-bold text-xl px-4">Who to follow</h4>
       {randomUser.map((randomUser) => (
         <div
