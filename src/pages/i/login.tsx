c170a2dc09e43f0ba3c65c141bbe00d8ffa6e547
kio445566 <87538436+kio445566@users.noreply.github.com>
2023-03-25 21:37:07 +0800

    add login and signup button in login and signup page

--- src/pages/i/login.tsx
+++ src/pages/i/login.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import useStorage from "../../hooks/useStorage";
 import { useRouter } from "next/router";
+import Widget from "@/components/Widget";
 
 export default function Login() {
   const router = useRouter();
@@ -30,47 +31,50 @@ export default function Login() {
   };
 
   return (
-    <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
-      <div className="flex py-2 px-3 sticky top-0  bg-white border-b border-gray-200">
-        <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Login</h2>
-      </div>
-      <div className="p-4">
-        <form>
-          <label className="block">
-            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-              Username:
-            </span>
+    <>
+      <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
+        <div className="flex py-2 px-3 sticky top-0  bg-white border-b border-gray-200">
+          <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Login</h2>
+        </div>
+        <div className="p-4">
+          <form>
+            <label className="block">
+              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                Username:
+              </span>
+              <input
+                type="text"
+                name="userId"
+                className="mb-3 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                placeholder="account"
+                onChange={handleUsernameChange}
+                required
+              />
+            </label>
+            <label className="block">
+              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                Password:
+              </span>
+              <input
+                type="password"
+                name="password"
+                className="mb-3 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                placeholder="password"
+                onChange={handlePasswordChange}
+                required
+              />
+            </label>
             <input
-              type="text"
-              name="userId"
-              className="mb-3 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              placeholder="account"
-              onChange={handleUsernameChange}
-              required
+              type="button"
+              className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 m-1"
+              value="Login"
+              onClick={handleSubmit}
             />
-          </label>
-          <label className="block">
-            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-              Password:
-            </span>
-            <input
-              type="password"
-              name="password"
-              className="mb-3 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              placeholder="password"
-              onChange={handlePasswordChange}
-              required
-            />
-          </label>
-          <input
-            type="button"
-            className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 m-1"
-            value="Login"
-            onClick={handleSubmit}
-          />
-          <p>{warning}</p>
-        </form>
+            <p>{warning}</p>
+          </form>
+        </div>
       </div>
-    </div>
+      <Widget />
+    </>
   );
 }
--- src/pages/i/signup.tsx
+++ src/pages/i/signup.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import useStorage from "../../hooks/useStorage";
+import Widget from "@/components/Widget";
 
 export default function Signup() {
   const { setItem } = useStorage();
@@ -20,52 +21,57 @@ export default function Signup() {
   const handleSubmit = () => {};
 
   return (
-    <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
-      <div className="flex py-2 px-3 sticky top-0  bg-white border-b border-gray-200">
-        <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Sign up</h2>
-      </div>
-      <div className="p-4">
-        <form>
-          <label className="block">
-            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-              Username:
-            </span>
+    <>
+      <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
+        <div className="flex py-2 px-3 sticky top-0  bg-white border-b border-gray-200">
+          <h2 className="text-lg sm:text-xl font-bold cursor-pointer">
+            Sign up
+          </h2>
+        </div>
+        <div className="p-4">
+          <form>
+            <label className="block">
+              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                Username:
+              </span>
+              <input
+                type="text"
+                name="userId"
+                className="mb-3 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                placeholder="account"
+                onChange={handleUsernameChange}
+                required
+              />
+            </label>
+            <label className="block">
+              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                Password:
+              </span>
+              <input
+                type="password"
+                name="password"
+                className="mb-3 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                placeholder="password"
+                onChange={handlePasswordChange}
+                required
+              />
+            </label>
             <input
-              type="text"
-              name="userId"
-              className="mb-3 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              placeholder="account"
-              onChange={handleUsernameChange}
-              required
+              type="button"
+              className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 m-1"
+              value="Sign up"
+              onClick={handleSubmit}
             />
-          </label>
-          <label className="block">
-            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-              Password:
-            </span>
-            <input
-              type="password"
-              name="password"
-              className="mb-3 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-              placeholder="password"
-              onChange={handlePasswordChange}
-              required
-            />
-          </label>
-          <input
-            type="button"
-            className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 m-1"
-            value="Sign up"
-            onClick={handleSubmit}
-          />
-          <p>{warning}</p>
-          <Link href="/">
-            <p id="autoclick" hidden>
-              go to home page
-            </p>
-          </Link>
-        </form>
+            <p>{warning}</p>
+            <Link href="/">
+              <p id="autoclick" hidden>
+                go to home page
+              </p>
+            </Link>
+          </form>
+        </div>
       </div>
-    </div>
+      <Widget />
+    </>
   );
 }
