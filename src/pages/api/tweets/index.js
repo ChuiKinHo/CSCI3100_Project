389adb8dd4b94a189f0dc21b97115c44c4f4a705
Nichole Chow <nicolechow08@gmail.com>
2023-04-10 17:35:28 +0800

    Fetch all post

---
+++ src/pages/api/tweets/index.js
@@ -0,0 +1,27 @@
+import dbConnect from "../../../_unsorted/database/dbConnect";
+import { Post } from "../../../_unsorted/database/schemas";
+import { pwd } from "../../../_unsorted/util/utils";
+import { Tweet } from "../../../_unsorted/database/schemas";
+
+export default async function handler(req, res) {
+  // Wait for database to connect before continuing
+  const { method } = req;
+
+  await dbConnect();
+
+  // api/users
+  switch (method) {
+    case "GET": // Get all Users
+      try {
+        const tweet = await Tweet.find({});
+        res.status(200).json({ success: true, data: tweet });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    default:
+      res.status(400).json({ success: false, data: "Invalid request!" });
+      break;
+  }
+}
