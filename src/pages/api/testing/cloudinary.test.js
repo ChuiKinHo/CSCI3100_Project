50ae2d8b3cac517535e110def892908a86c86405
tanjaimpens <79149859+tanjaimpens@users.noreply.github.com>
2023-04-11 22:42:55 +0800

    working cloudinary upload :)

--- src/pages/api/testing/cloudinary.test.js
+++ src/pages/api/testing/cloudinary.test.js
@@ -3,13 +3,8 @@ import fs from "fs";
 import path from 'path';
 
 export default async function handler(req, res) {
-  const imagePath = path.join(__dirname, 'yes.jpeg');
-  const imageData = fs.readFileSync('C:\\Users\\Tanja\\Downloads\\Software Engineering\\project\\CSCI3100_Project\\src\\pages\\api\\testing\\yes.jpeg');
-  
-
-
   // Call uploadImage function with the image buffer
-  uploadImage(imageData)
+  uploadImage('C:\\Users\\Tanja\\Downloads\\Software Engineering\\project\\CSCI3100_Project\\src\\pages\\api\\testing\\yes.jpeg')
     .then(url => {
       console.log(url);
       res.status(200).json({ url });
