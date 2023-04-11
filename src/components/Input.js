5b03ba365e8bdd1c601172dc0a69ac51d0ce9c8d
Nichole Chow <nicolechow08@gmail.com>
2023-04-11 20:45:19 +0800

    Sample data removed for input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,15 +2,14 @@ import { FaceSmileIcon, PhotoIcon } from "@heroicons/react/20/solid";
 import useStorage from "../hooks/useStorage";
 import { useEffect, useState } from "react";
 import { userImg } from "../_unsorted/imageRelated/cloudinary/utils";
-
-// TODO:: Remove sample data
-import users from "@/data/sampleUsers.json";
+import { useRef } from "react";
 
 export default function Input() {
   const { getItem, removeItem } = useStorage();
   const [username, setUsername] = useState(null);
   const [isAdmin, setIsAdmin] = useState(1);
   const [userInfo, setUserInfo] = useState(null);
+  const isMountedRef = useRef(false);
 
   useEffect(() => {
     setUsername(getItem("username", "session"));
@@ -40,54 +39,59 @@ export default function Input() {
     }
   }, [username]);
 
-  // const { getItem } = useStorage();
-  // const username = getItem("username", "session");
-  // const [userImg, setUserImg] = useState(
-  //   "https://twirpz.files.wordpress.com/2015/06/twitter-avi-gender-balanced-figure.png"
-  // );
   useEffect(() => {
     if (username != null) {
-      // TODO:: Remove sample data
-      let user = users.find((user) => user.username === username);
-      if (user != null && user.userImg != null && user.userImg != "") {
-        setUserInfo(user.userImg);
-      }
+      fetch("http://localhost:3000/api/users?q=@" + username, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setUserInfo(data.data);
+          isMountedRef.current = true;
+
+          return () => {
+            isMountedRef.current = false;
+          };
+        })
+        .catch((error) => {
+          console.error("Error fetching posts:", error);
+        });
     }
-  }, []);
-  return (
-    <div className="flex border-b border-gray-200 p-3 space-x-3">
-      {/* <img
-        src={userImg}
-        alt="user-img"
-        className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
-      /> */}
-      <div>{userImg(userInfo)}</div>
+  }, [username]);
+  if (isMountedRef && userInfo != null) {
+    return (
+      <div className="flex border-b border-gray-200 p-3 space-x-3">
+        <div>{userImg(userInfo)}</div>
 
-      <div className="w-full divide-y divide-gray-200">
-        <div className="flex-1">
-          <textarea
-            className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700 resize-none"
-            rows="2"
-            placeholder="What's happening?"
-          ></textarea>
-        </div>
-
-        <div className="flex items-center justify-between pt-2.5">
-          <div className="flex">
-            <div className="">
-              <label htmlFor="image-upload">
-                <PhotoIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
-              </label>
-              <input id="image-upload" type="file" hidden />
-            </div>
-            <FaceSmileIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+        <div className="w-full divide-y divide-gray-200">
+          <div className="flex-1">
+            <textarea
+              className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700 resize-none"
+              rows="2"
+              placeholder="What's happening?"
+            ></textarea>
           </div>
 
-          <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
-            Tweet
-          </button>
+          <div className="flex items-center justify-between pt-2.5">
+            <div className="flex">
+              <div className="">
+                <label htmlFor="image-upload">
+                  <PhotoIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                </label>
+                <input id="image-upload" type="file" hidden />
+              </div>
+              <FaceSmileIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+            </div>
+
+            <button className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50">
+              Tweet
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
