57db0435e0fd9a43fe81ae98c787b461289cf227
Nichole Chow <nicolechow08@gmail.com>
2023-04-12 23:03:30 +0800

    Basic retweet UI

--- src/components/Post.js
+++ src/components/Post.js
@@ -12,12 +12,14 @@ import React, { useState, useEffect } from "react";
 import ActionBar from "@/components/ActionBar";
 import useStorage from "../hooks/useStorage";
 import { userImg } from "../_unsorted/imageRelated/cloudinary/utils";
+import Retweet from "./Retweet";
 
 export default function Post({ id }) {
   const { getItem, removeItem } = useStorage();
   const [username, setUsername] = useState(null);
   const [isAdmin, setIsAdmin] = useState(1);
   const [post, setPost] = useState(null);
+  const [retweet, setRetweet] = useState(null);
 
   useEffect(() => {
     setUsername(getItem("username", "session"));
@@ -50,23 +52,41 @@ export default function Post({ id }) {
     }
   }, [id, username]);
 
+  useEffect(() => {
+    if (post !== null && post.retweet && id !== null) {
+      fetch("http://localhost:3000/api/retweets?tweetid=" + id, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data !== null) {
+            setRetweet(data.data);
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching posts:", error);
+        });
+    }
+  }, [id, post]);
+
   if (post !== null) {
+    console.log(retweet);
     return (
       <>
-        {post.retweet && (
-          <div className="flex items-center">
-            <ArrowPathRoundedSquareIcon className="h-4 w-4" />
-            <span className="ml-1 text-gray-500">Retweeted</span>
-          </div>
-        )}
+        {retweet !== null && (
+          <>
+            <div className="flex items-center">
+              <ArrowPathRoundedSquareIcon className="h-4 w-4" />
+              <span className="ml-1 text-gray-500">Retweeted</span>
+            </div>
 
-        <div className="flex p-3 cursor-pointer border-b border-gray-200">
-          <Link href={"/" + post.userObjectId.username}>
-            {userImg(post.userObjectId)}
-          </Link>
-
-          <div className="flex-1">
             <div className="flex items-center justify-between">
+              <Link href={"/" + post.userObjectId.username}>
+                {userImg(post.userObjectId)}
+              </Link>
               <div className="flex items-center space-x-1 whitespace-nowrap">
                 <Link href={"/" + post.userObjectId.username}>
                   <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
@@ -93,13 +113,88 @@ export default function Post({ id }) {
               <EllipsisHorizontalCircleIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
             </div>
 
-            <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
-              {post.text}
-            </p>
-            <img className="w-full rounded-lg" src={post?.img} alt="" />
-            <ActionBar post={post} />
+            <div className="flex p-3 cursor-pointer border-b border-gray-200">
+              <Link href={"/" + retweet.userObjectId.username}>
+                {userImg(retweet.userObjectId)}
+              </Link>
+
+              <div className="flex-1">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center space-x-1 whitespace-nowrap">
+                    <Link href={"/" + retweet.userObjectId.username}>
+                      <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+                        {retweet.userObjectId.name}
+                      </h4>
+                    </Link>
+                    <span className="text-sm sm:text-[15px]">
+                      @{retweet.userObjectId.username} -{" "}
+                    </span>
+                    <span className="text-sm sm:text-[15px]">
+                      {new Intl.RelativeTimeFormat("en", {
+                        numeric: "auto",
+                      }).format(
+                        -Math.floor(
+                          (new Date() - new Date(retweet.timestamp)) /
+                            (1000 * 60 * 60 * 24)
+                        ),
+                        "day"
+                      )}
+                    </span>
+                  </div>
+                </div>
+
+                <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
+                  {retweet.text}
+                </p>
+                <img className="w-full rounded-lg" src={retweet?.img} alt="" />
+                <ActionBar post={post} />
+              </div>
+            </div>
+          </>
+        )}
+
+        {!post.retweet && (
+          <div className="flex p-3 cursor-pointer border-b border-gray-200">
+            <Link href={"/" + post.userObjectId.username}>
+              {userImg(post.userObjectId)}
+            </Link>
+
+            <div className="flex-1">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center space-x-1 whitespace-nowrap">
+                  <Link href={"/" + post.userObjectId.username}>
+                    <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+                      {post.userObjectId.name}
+                    </h4>
+                  </Link>
+                  <span className="text-sm sm:text-[15px]">
+                    @{post.userObjectId.username} -{" "}
+                  </span>
+                  <span className="text-sm sm:text-[15px]">
+                    {new Intl.RelativeTimeFormat("en", {
+                      numeric: "auto",
+                    }).format(
+                      -Math.floor(
+                        (new Date() - new Date(post.timestamp)) /
+                          (1000 * 60 * 60 * 24)
+                      ),
+                      "day"
+                    )}
+                  </span>
+                </div>
+
+                {/*TODO:: Turn this into button */}
+                <EllipsisHorizontalCircleIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
+              </div>
+
+              <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
+                {post.text}
+              </p>
+              <img className="w-full rounded-lg" src={post?.img} alt="" />
+              <ActionBar post={post} />
+            </div>
           </div>
-        </div>
+        )}
       </>
     );
   }
