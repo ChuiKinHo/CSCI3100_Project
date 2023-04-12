f9bd8d44bb2fcef4347fb93ee37b88c056c426ee
Nichole Chow <nicolechow08@gmail.com>
2023-04-12 14:47:48 +0800

    Created following and follower page

---
+++ src/pages/[username]/following/index.js
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+import Post from "@/components/Post";
+import Widget from "@/components/Widget";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+import { userImgProfile } from "@/_unsorted/imageRelated/cloudinary/utils";
+
+export default function followingPage() {}
