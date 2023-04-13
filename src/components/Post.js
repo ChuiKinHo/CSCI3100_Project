0881ec1a0e60eda0b4f371db988fc66ee6885008
kio445566 <87538436+kio445566@users.noreply.github.com>
2023-04-13 13:14:07 +0800

    fixed post link

--- src/components/Post.js
+++ src/components/Post.js
@@ -177,46 +177,48 @@ export default function Post({ id }) {
         )}
 
         {!post.retweet && (
-          <div className="flex p-3 cursor-pointer border-b border-gray-200 hover:bg-gray-100">
-            <Link href={"/" + post.userObjectId.username}>
-              {userImg(post.userObjectId)}
-            </Link>
+          <Link href={"/" + post.userObjectId.username + "/status/" + post.id}>
+            <div className="flex p-3 cursor-pointer border-b border-gray-200 hover:bg-gray-100">
+              <Link href={"/" + post.userObjectId.username}>
+                {userImg(post.userObjectId)}
+              </Link>
 
-            <div className="flex-1">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center space-x-1 whitespace-nowrap">
-                  <Link href={"/" + post.userObjectId.username}>
-                    <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
-                      {post.userObjectId.name}
-                    </h4>
-                  </Link>
-                  <span className="text-sm sm:text-[15px]">
-                    @{post.userObjectId.username} -{" "}
-                  </span>
-                  <span className="text-sm sm:text-[15px]">
-                    {new Intl.RelativeTimeFormat("en", {
-                      numeric: "auto",
-                    }).format(
-                      -Math.floor(
-                        (new Date() - new Date(post.timestamp)) /
-                          (1000 * 60 * 60 * 24)
-                      ),
-                      "day"
-                    )}
-                  </span>
+              <div className="flex-1">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center space-x-1 whitespace-nowrap">
+                    <Link href={"/" + post.userObjectId.username}>
+                      <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+                        {post.userObjectId.name}
+                      </h4>
+                    </Link>
+                    <span className="text-sm sm:text-[15px]">
+                      @{post.userObjectId.username} -{" "}
+                    </span>
+                    <span className="text-sm sm:text-[15px]">
+                      {new Intl.RelativeTimeFormat("en", {
+                        numeric: "auto",
+                      }).format(
+                        -Math.floor(
+                          (new Date() - new Date(post.timestamp)) /
+                            (1000 * 60 * 60 * 24)
+                        ),
+                        "day"
+                      )}
+                    </span>
+                  </div>
+
+                  {/*TODO:: Turn this into button */}
+                  <EllipsisHorizontalCircleIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
                 </div>
 
-                {/*TODO:: Turn this into button */}
-                <EllipsisHorizontalCircleIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
+                <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
+                  {post.text}
+                </p>
+                <img className="w-full rounded-lg" src={post?.img} alt="" />
+                <ActionBar post={post} />
               </div>
-
-              <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
-                {post.text}
-              </p>
-              <img className="w-full rounded-lg" src={post?.img} alt="" />
-              <ActionBar post={post} />
             </div>
-          </div>
+          </Link>
         )}
       </>
     );
