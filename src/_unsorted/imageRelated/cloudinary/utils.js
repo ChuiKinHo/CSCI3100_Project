49e1f8727f70dc42ceff55d0526bf00347f07c88
Nichole Chow <nicolechow08@gmail.com>
2023-04-13 11:38:33 +0800

    Delete

--- src/_unsorted/imageRelated/cloudinary/utils.js
+++ src/_unsorted/imageRelated/cloudinary/utils.js
@@ -5,12 +5,10 @@ export function UploadButton() {
   return (
     <>
       <CldUploadButton
-        // className={styles.button}
         onUpload={(error, result, widget) => {
           setResource(result?.info); // Updating local state with asset details
           widget.close(); // Close widget immediately after successful upload
         }}
-        signatureEndpoint="/api/sign-cloudinary-params"
         uploadPreset="next-cloudinary-signed"
       >
         Upload to Cloudinary
