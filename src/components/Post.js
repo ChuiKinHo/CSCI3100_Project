d5554b0860d9cc04957be9c291339f0983caefc0
kio445566 <87538436+kio445566@users.noreply.github.com>
2023-04-13 18:44:12 +0800

    fix post link region

--- src/components/Post.js
+++ src/components/Post.js
@@ -78,118 +78,20 @@ export default function Post({ id }) {
     return (
       <>
         {retweet !== null && (
-          <Link href={"/" + post.userObjectId.username + "/status/" + post.id}>
+          <div>
             <div className="hover:bg-gray-100">
-              <div className="flex items-center">
-                <ArrowPathRoundedSquareIcon className="h-4 w-4" />
-                <span className="ml-1 text-gray-500">Retweeted</span>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <Link href={"/" + post.userObjectId.username}>
-                  {userImg(post.userObjectId)}
-                </Link>
-                <div className="flex items-center space-x-1 whitespace-nowrap">
-                  <Link href={"/" + post.userObjectId.username}>
-                    <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
-                      {post.userObjectId.name}
-                    </h4>
-                  </Link>
-                  <span className="text-sm sm:text-[15px]">
-                    @{post.userObjectId.username} -{" "}
-                  </span>
-                  <span className="text-sm sm:text-[15px]">
-                    {new Intl.RelativeTimeFormat("en", {
-                      numeric: "auto",
-                    }).format(
-                      -Math.floor(
-                        (new Date() - new Date(post.timestamp)) /
-                          (1000 * 60 * 60 * 24)
-                      ),
-                      "day"
-                    )}
-                  </span>
+              <Link
+                href={"/" + post.userObjectId.username + "/status/" + post.id}
+              >
+                <div className="flex items-center">
+                  <ArrowPathRoundedSquareIcon className="h-4 w-4" />
+                  <span className="ml-1 text-gray-500">Retweeted</span>
                 </div>
 
-                {/*TODO:: Turn this into button */}
-                <EllipsisHorizontalCircleIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
-              </div>
-
-              <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
-                {post.text}
-              </p>
-
-              <div className="border-b border-gray-200">
-                <Link
-                  href={
-                    "/" +
-                    retweet.userObjectId.username +
-                    "/status/" +
-                    retweet.id
-                  }
-                >
-                  <div className="flex p-3 cursor-pointer border border-gray-200 m-2 hover:bg-gray-200">
-                    <Link href={"/" + retweet.userObjectId.username}>
-                      {userImg(retweet.userObjectId)}
-                    </Link>
-
-                    <div className="flex-1">
-                      <div className="flex items-center justify-between">
-                        <div className="flex items-center space-x-1 whitespace-nowrap">
-                          <Link href={"/" + retweet.userObjectId.username}>
-                            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
-                              {retweet.userObjectId.name}
-                            </h4>
-                          </Link>
-                          <span className="text-sm sm:text-[15px]">
-                            @{retweet.userObjectId.username} -{" "}
-                          </span>
-                          <span className="text-sm sm:text-[15px]">
-                            {new Intl.RelativeTimeFormat("en", {
-                              numeric: "auto",
-                            }).format(
-                              -Math.floor(
-                                (new Date() - new Date(retweet.timestamp)) /
-                                  (1000 * 60 * 60 * 24)
-                              ),
-                              "day"
-                            )}
-                          </span>
-                        </div>
-                      </div>
-
-                      <div>
-                        <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
-                          {retweet.text}
-                        </p>
-                        {retweet?.img && (
-                          <CldImage
-                            width={700}
-                            height={700}
-                            crop="fill"
-                            src={retweet?.img}
-                            alt={retweet?.img}
-                          />
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-                <ActionBar post={post} />
-              </div>
-            </div>
-          </Link>
-        )}
-
-        {!post.retweet && (
-          <Link href={"/" + post.userObjectId.username + "/status/" + post.id}>
-            <div className="flex p-3 cursor-pointer border-b border-gray-200 hover:bg-gray-100">
-              <Link href={"/" + post.userObjectId.username}>
-                {userImg(post.userObjectId)}
-              </Link>
-
-              <div className="flex-1">
                 <div className="flex items-center justify-between">
+                  <Link href={"/" + post.userObjectId.username}>
+                    {userImg(post.userObjectId)}
+                  </Link>
                   <div className="flex items-center space-x-1 whitespace-nowrap">
                     <Link href={"/" + post.userObjectId.username}>
                       <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
@@ -219,19 +121,125 @@ export default function Post({ id }) {
                 <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
                   {post.text}
                 </p>
-                {post?.img && (
-                  <CldImage
-                    width={700}
-                    height={700}
-                    crop="fill"
-                    src={post?.img}
-                    alt={post?.img}
-                  />
-                )}
-                <ActionBar post={post} />
-              </div>
+
+                <div className="border-b border-gray-200">
+                  <Link
+                    href={
+                      "/" +
+                      retweet.userObjectId.username +
+                      "/status/" +
+                      retweet.id
+                    }
+                  >
+                    <div className="flex p-3 cursor-pointer border border-gray-200 m-2 hover:bg-gray-200">
+                      <Link href={"/" + retweet.userObjectId.username}>
+                        {userImg(retweet.userObjectId)}
+                      </Link>
+
+                      <div className="flex-1">
+                        <div className="flex items-center justify-between">
+                          <div className="flex items-center space-x-1 whitespace-nowrap">
+                            <Link href={"/" + retweet.userObjectId.username}>
+                              <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+                                {retweet.userObjectId.name}
+                              </h4>
+                            </Link>
+                            <span className="text-sm sm:text-[15px]">
+                              @{retweet.userObjectId.username} -{" "}
+                            </span>
+                            <span className="text-sm sm:text-[15px]">
+                              {new Intl.RelativeTimeFormat("en", {
+                                numeric: "auto",
+                              }).format(
+                                -Math.floor(
+                                  (new Date() - new Date(retweet.timestamp)) /
+                                    (1000 * 60 * 60 * 24)
+                                ),
+                                "day"
+                              )}
+                            </span>
+                          </div>
+                        </div>
+
+                        <div>
+                          <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
+                            {retweet.text}
+                          </p>
+                          {retweet?.img && (
+                            <CldImage
+                              width={700}
+                              height={700}
+                              crop="fill"
+                              src={retweet?.img}
+                              alt={retweet?.img}
+                            />
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              </Link>
             </div>
-          </Link>
+            <ActionBar post={post} />
+          </div>
+        )}
+
+        {!post.retweet && (
+          <div className="border-b border-gray-200">
+            <Link
+              href={"/" + post.userObjectId.username + "/status/" + post.id}
+            >
+              <div className="flex p-3 cursor-pointer hover:bg-gray-100">
+                <Link href={"/" + post.userObjectId.username}>
+                  {userImg(post.userObjectId)}
+                </Link>
+
+                <div className="flex-1">
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center space-x-1 whitespace-nowrap">
+                      <Link href={"/" + post.userObjectId.username}>
+                        <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+                          {post.userObjectId.name}
+                        </h4>
+                      </Link>
+                      <span className="text-sm sm:text-[15px]">
+                        @{post.userObjectId.username} -{" "}
+                      </span>
+                      <span className="text-sm sm:text-[15px]">
+                        {new Intl.RelativeTimeFormat("en", {
+                          numeric: "auto",
+                        }).format(
+                          -Math.floor(
+                            (new Date() - new Date(post.timestamp)) /
+                              (1000 * 60 * 60 * 24)
+                          ),
+                          "day"
+                        )}
+                      </span>
+                    </div>
+
+                    {/*TODO:: Turn this into button */}
+                    <EllipsisHorizontalCircleIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
+                  </div>
+
+                  <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
+                    {post.text}
+                  </p>
+                  {post?.img && (
+                    <CldImage
+                      width={700}
+                      height={700}
+                      crop="fill"
+                      src={post?.img}
+                      alt={post?.img}
+                    />
+                  )}
+                </div>
+              </div>
+            </Link>
+            <ActionBar post={post} />
+          </div>
         )}
       </>
     );
