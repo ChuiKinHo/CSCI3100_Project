f3e16af47095f7f141303f2b3b8c27752b978034
Nichole Chow <nicolechow08@gmail.com>
2023-05-05 12:13:56 +0800

    Update getAllUsers.js

--- src/pages/api/admin/getAllUsers.js
+++ src/pages/api/admin/getAllUsers.js
@@ -7,29 +7,31 @@ export default async function handler(req, res) {
 
   await dbConnect();
 
-  // api/users
+  // Handle GET requests to retrieve a list of users
+  //api/users
   switch (method) {
     case "GET":
       try {
+        // Retrieve all users from the database and populate the follower, following, and mytweets fields for each user
         const users = await User.find()
-        .populate({
-          path: "follower",
-          select: "username",
-          populate: {
-            path: "following",
-            model: "User",
-            select: "username",
-          },
-        })
-        .populate({
-          path: "following",
-          select: "username",
-          populate: {
+          .populate({
             path: "follower",
-            model: "User",
             select: "username",
-          },
-        })
+            populate: {
+              path: "following",
+              model: "User",
+              select: "username",
+            },
+          })
+          .populate({
+            path: "following",
+            select: "username",
+            populate: {
+              path: "follower",
+              model: "User",
+              select: "username",
+            },
+          })
           .populate({
             path: "mytweets",
             populate: {
@@ -38,14 +40,16 @@ export default async function handler(req, res) {
             },
           });
 
-
+        // Send the retrieved users as a JSON response
         res.status(200).json({ success: true, data: users });
       } catch (error) {
+        // Log and send an error response if an error occurs
         res.status(400).json({ success: false, data: error });
       }
       break;
 
     default:
+      // Send an error response for invalid requests
       res.status(400).json({ success: false, data: "Invalid request!" });
       break;
   }
