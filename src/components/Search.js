f1005e2656fc75ee7ecec09935926d1cc97e464f
Nichole Chow <nicolechow08@gmail.com>
2023-05-06 13:58:24 +0800

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,26 +4,28 @@
  * Author: Chui Kin Ho, Chow Tsz Ching, Dingcheng Wang, Heung Tsz Kit, Tanja Impens
  * Date: May  5 2023, 11:08:51 PM
  * Version: 1.0
- * Description:
+ * Description: serach bar
  * -----------------------------
  */
+// Importing required libraries and components
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
+// Defining a React functional component named "Search"
 export default function Search() {
+  // Initializing "router" and "query" state variable
   const router = useRouter();
   const [query, setQuery] = useState("");
 
+  // Function to handle form submission and search query updates
   const handleSubmit = (e) => {
     e.preventDefault();
     router.push(`/query/${query}`);
   };
+  const handleChange = (e) => setQuery(e.target.value);
 
-  const handleChange = (e) => {
-    setQuery(e.target.value);
-  };
-
+  // Rendering the search bar UI with search icon and input field
   return (
     <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5">
       <form
